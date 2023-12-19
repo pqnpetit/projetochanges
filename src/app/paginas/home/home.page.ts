221cf9a7a7a62b7326dc4670d01b5fe66a518f08
pqnpetit <pqnpetit@gmail.com>
2023-12-19 14:58:56 -0300

    actual

--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -1,4 +1,7 @@
+// home.page.ts
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  posts: any[] = [];
+  
+  // Declare a propriedade newComment
+  newComment = ''; // Variável para armazenar o novo comentário
 
-  ngOnInit() {
+  constructor(private authService: ApiService) { }
+
+  ngOnInit() {}
+
+  doRefresh(event: { target: { complete: () => void; }; }) {
+    setTimeout(() => {
+      console.log('Operação de atualização completa');
+      event.target.complete();
+    }, 2000);
   }
 
+  addComment(post: any) {
+    // Adicionar lógica para adicionar um comentário ao post
+    // Aqui você pode chamar o serviço de autenticação para adicionar um comentário com o usuário autenticado
+  }
+
+
+
+  
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,12 +7,26 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private apiUrl = 'http://localhost:3000'; // Altere para o URL correto do seu backend
-
+  private loginUrl = 'http://localhost:3000/login';
   constructor(private http: HttpClient) {}
 
   cadastrarUsuario(usuario: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, usuario);
   }
 
+
+  authenticate(username: string, password: string): Observable<any> {
+    return this.http.get<any[]>(this.loginUrl, { observe: 'response' });
+  }
+
+    // Buscar todos os posts
+     getUsersWithPosts(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/login`);
+  }
+  
+    // Adicionar um comentário a um post específico
+    addCommentToPost(postId: number, comment: any): Observable<any> {
+      return this.http.post(`${this.apiUrl}/posts/${postId}/comments`, comment);
+    }
   // Adicione outras operações CRUD ou métodos necessários aqui
 }
