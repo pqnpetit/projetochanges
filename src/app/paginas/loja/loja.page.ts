a3a1a43dd24494242f4d4d76fd38bbbad8e3d6b0
pqnpetit <pqnpetit@gmail.com>
2023-12-20 11:06:01 -0300

    atual

--- src/app/paginas/loja/loja.page.ts
+++ src/app/paginas/loja/loja.page.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./loja.page.scss'],
 })
 export class LojaPage {
-  product: any = {
-    url_da_imagem: "/assets/img/ondas.jpg", // Insira aqui a URL da imagem
-    nome: 'Nome do Produto',
-    descricao: 'Descrição do Produto'
-  };
+  
 
   buyProduct(product: any) {
     console.log('Produto comprado:', product);
